b4ab399a7b15d32b3bb2f75ba65a3fa4d8d8756c
Leandro Gomes - E. Escola de T.I - BNE <cesarsilva@bne.com.br>
2023-10-29 17:40:22 -0300

    Add: criando metodos da interface e do repository

---
+++ Infra/Repositorio/RepositorioDespesa.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Interfaces.Generics;
+using Domain.Interfaces.IDespesa;
+using Entities.Entidades;
+using Infra.Repositorio.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repositorio
+{
+    public class RepositorioDespesa : RepositoryGenerics<Despesa>, InterfaceDespesa
+    {
+        public Task<IList<Despesa>> ListarDespesaUsuario(string emailUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<Despesa>> ListarDespesaUsuarioNãoPagasMesesAnterior(string emailUsuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Infra/Repositorio/RepositorioUsuarioSistemaFinanceiro.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Interfaces.IUsuarioSistemaFinanceiro;
+using Entities.Entidades;
+using Infra.Repositorio.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Repositorio
+{
+    public class RepositorioUsuarioSistemaFinanceiro : RepositoryGenerics<UsuarioSistemaFinanceiro>, InterfaceUsuarioSistemaFinanceiro
+    {
+        public Task<IList<UsuarioSistemaFinanceiro>> ListaUsuariosSistema(int IdSistema)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsuarioSistemaFinanceiro> ObterUsuarioPorEmail(string emailUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoverUsuarios(List<UsuarioSistemaFinanceiro> usuarios)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
